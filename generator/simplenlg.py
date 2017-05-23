1c4418e4295e7d1b36f50ba3e25d990d48186a7a
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2017-05-23 10:22:30 -0300

    Improvements in easy nlg system

--- generator/simplenlg.py
+++ generator/simplenlg.py
@@ -16,6 +16,75 @@ import nltk
 import operator
 import utils
 
+class EasyNLG(object):
+    def __init__(self):
+        self.references = []
+        self.hyps = []
+
+        deventries = Entry.objects(set='dev')
+        for deventry in deventries:
+            self.process(deventry)
+
+    def extract_template(self, triples):
+        # entity and predicate mapping
+        entitymap, predicates = utils.map_entities(triples=triples)
+
+        trainentries = Entry.objects(size=len(triples), set='train')
+        for i, triple in enumerate(triples):
+            trainentries = filter(lambda entry: entry.triples[i].predicate.name == triple.predicate.name, trainentries)
+
+        # extract templates
+        templates = []
+        for entry in trainentries:
+            for lexEntry in entry.texts:
+                template = lexEntry.template
+
+                entitiesPresence = True
+                for tag in entitymap:
+                    if tag not in template:
+                        entitiesPresence = False
+                        break
+                if entitiesPresence:
+                    templates.append(template)
+        templates = nltk.FreqDist(templates)
+        item = sorted(templates.items(), key=operator.itemgetter(1), reverse=True)
+        if len(item) == 0:
+            template, freq = '', 0
+        else:
+            template, freq = item[0]
+            for tag, name in entitymap.iteritems():
+                template = template.replace(tag, ' '.join(name.replace('\'', '').replace('\"', '').split('_')))
+        return template, entitymap, predicates
+
+    def process(self, deventry):
+        # extract references
+        refs = []
+        for lexEntry in deventry.texts:
+            text = lexEntry.text
+            refs.append(text)
+        self.references.append(refs)
+
+        template, entitymap, predicates = '', {}, []
+        i = len(deventry.triples)
+        while template == '' or i > 0:
+            triple1, triple2 = deventry.triples[:i], deventry.triples[i:]
+
+            template1, entitymap, predicates = self.extract_template(triple1)
+            template2, entitymap, predicates = self.extract_template(triple2)
+
+            template = template1 + ' ' + template2
+            template = template.strip()
+            i = i - 1
+
+        self.hyps.append(template)
+
+        print 10 * '-'
+        print 'Entities: ', str(entitymap)
+        print 'Predicate: ', str(predicates)
+        print template.encode('utf-8')
+        print 10 * '-'
+        return template
+
 def write_references(refs, fname):
     f1 = open(fname+'1', 'w')
     f2 = open(fname+'2', 'w')
@@ -56,58 +125,6 @@ def write_hyps(hyps, fname):
         f.write('\n')
     f.close()
 
-def main():
-    deventries = Entry.objects(set='dev')
-
-    references, hyps = [], []
-
-    for deventry in deventries:
-        # entity and predicate mapping
-        entitymap, predicates = utils.map_entities(triples=deventry.triples)
-
-        trainentries = Entry.objects(size=len(deventry.triples), set='train')
-        for i, triple in enumerate(deventry.triples):
-            trainentries = filter(lambda entry: entry.triples[i].predicate.name == triple.predicate.name, trainentries)
-
-        # extract references
-        refs = []
-        for lexEntry in deventry.texts:
-            text = lexEntry.text
-            refs.append(text)
-        references.append(refs)
-
-        # extract templates
-        templates = []
-        for entry in trainentries:
-            for lexEntry in entry.texts:
-                template = lexEntry.template
-
-                entitiesPresence = True
-                for tag in entitymap:
-                    if tag not in template:
-                        entitiesPresence = False
-                        break
-                if entitiesPresence:
-                    templates.append(template)
-        templates = nltk.FreqDist(templates)
-
-        item = sorted(templates.items(), key=operator.itemgetter(1), reverse=True)
-        if len(item) == 0:
-            template, freq = '', 0
-        else:
-            template, freq = item[0]
-            for tag, name in entitymap.iteritems():
-                template = template.replace(tag, ' '.join(name.split('_')))
-
-        print 10 * '-'
-        print 'Entities: ', str(entitymap)
-        print 'Predicate: ', str(predicates)
-        print template.encode('utf-8'), freq
-        print 10 * '-'
-
-        hyps.append(template)
-    return hyps, references
-
 if __name__ == '__main__':
     # python simplenlg.py /home/tcastrof/cyber/data/easy_nlg/hyps /home/tcastrof/cyber/data/easy_nlg/ref
     parser = argparse.ArgumentParser()
@@ -115,7 +132,7 @@ if __name__ == '__main__':
     parser.add_argument('refs', type=str, default='/home/tcastrof/cyber/data/easy_nlg/ref', help='references writing file')
     args = parser.parse_args()
 
-    hyps, references = main()
+    nlg = EasyNLG()
 
-    write_references(references, args.refs)
-    write_hyps(hyps, args.hyps)
+    write_references(nlg.references, args.refs)
+    write_hyps(nlg.hyps, args.hyps)
