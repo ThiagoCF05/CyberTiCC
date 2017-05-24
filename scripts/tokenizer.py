6cc91bf70fbb5fb30fb15e619f4f268cec4f208c
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2017-05-24 04:15:18 -0300

    option to split instead tokenizing

--- scripts/tokenizer.py
+++ scripts/tokenizer.py
@@ -15,14 +15,19 @@ sys.setdefaultencoding('utf8')
 import nltk
 import os
 
-def tokenize(fread, fwrite):
+def tokenize(fread, fwrite, useNLTK=False):
     f = open(fread)
     texts = f.read().split('\n')
     f.close()
 
     f = open(fwrite, 'w')
     for text in texts:
-        tokens = u' '.join(nltk.word_tokenize(text.lower()))
+        if useNLTK:
+            tokens = nltk.word_tokenize(text.lower())
+        else:
+            tokens = text.lower().split()
+        tokens = u' '.join(tokens)
+
         f.write(tokens)
         f.write('\n')
     f.close()
@@ -34,39 +39,40 @@ if __name__ == '__main__':
     parser.add_argument('directory', type=str, default='/home/tcastrof/cyber/data/easy_nlg', help='directory')
     parser.add_argument('--fread', type=str, default='', help='file to read')
     parser.add_argument('--fwrite', type=str, default='', help='file to write')
+    parser.add_argument("--nltk", action="store_true", help="NLTK tokenizer")
     args = parser.parse_args()
 
     if args.fread == '' and args.fwrite == '':
         print 'Hypothesis'
         fread = os.path.join(args.directory, 'hyps')
         fwrite = os.path.join(args.directory, 'hyps_tok')
-        tokenize(fread, fwrite)
+        tokenize(fread, fwrite, args.nltk)
 
         print 'Reference 1'
         fread = os.path.join(args.directory, 'ref1')
         fwrite = os.path.join(args.directory, 'ref1_tok')
-        tokenize(fread, fwrite)
+        tokenize(fread, fwrite, args.nltk)
 
         print 'Reference 2'
         fread = os.path.join(args.directory, 'ref2')
         fwrite = os.path.join(args.directory, 'ref2_tok')
-        tokenize(fread, fwrite)
+        tokenize(fread, fwrite, args.nltk)
 
         print 'Reference 3'
         fread = os.path.join(args.directory, 'ref3')
         fwrite = os.path.join(args.directory, 'ref3_tok')
-        tokenize(fread, fwrite)
+        tokenize(fread, fwrite, args.nltk)
 
         print 'Reference 4'
         fread = os.path.join(args.directory, 'ref4')
         fwrite = os.path.join(args.directory, 'ref4_tok')
-        tokenize(fread, fwrite)
+        tokenize(fread, fwrite, args.nltk)
 
         print 'Reference 5'
         fread = os.path.join(args.directory, 'ref5')
         fwrite = os.path.join(args.directory, 'ref5_tok')
-        tokenize(fread, fwrite)
+        tokenize(fread, fwrite, args.nltk)
     else:
         fread = args.fread
         fwrite = args.fwrite
-        tokenize(fread, fwrite)
+        tokenize(fread, fwrite, args.nltk)
