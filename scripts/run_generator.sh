46bbb61cdf0cf39eab9b3078c5b05a555ce150e9
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2017-08-09 03:56:16 -0300

    run generator

--- scripts/run_generator.sh
+++ scripts/run_generator.sh
@@ -5,4 +5,18 @@ python pronoun.py
 python proper_name.py
 
 cd ../
-python cybernlg.py /home/tcastrof/cyber/data/new_nlg_reg/hyps /home/tcastrof/cyber/data/new_nlg_reg/ref
+rm -R /home/tcastrof/cyber/data/template/manual
+mkdir /home/tcastrof/cyber/data/template/manual
+
+python cybernlg.py \
+	/home/tcastrof/cyber/data/template/manual/hyps \
+	/home/tcastrof/cyber/data/template/manual/ref \
+	manual
+
+rm -R /home/tcastrof/cyber/data/template/automatic
+mkdir /home/tcastrof/cyber/data/template/automatic
+
+python cybernlg.py \
+	/home/tcastrof/cyber/data/template/automatic/hyps \
+	/home/tcastrof/cyber/data/template/automatic/ref \
+	 automatic+manual
