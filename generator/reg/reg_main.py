6e2c1608b09a01cc8944ab7bf7db7eaab1f3ea06
Thiago Castro Ferreira <thiago.castro.ferreira@gmail.com>
2017-08-12 15:32:45 -0300

    utf-8 123

--- generator/reg/reg_main.py
+++ generator/reg/reg_main.py
@@ -118,7 +118,7 @@ class REG(object):
             prev_references.append(reference)
 
         for reference in references:
-            template = template.replace(reference['tag'], str(reference['realization']), 1)
+            template = str(template).replace(reference['tag'], str(reference['realization']), 1)
 
         return template
 
